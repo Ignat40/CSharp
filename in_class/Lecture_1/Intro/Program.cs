8a48e272a3a85bd5f1b3166b7eaa378df9381ecf
Ignat Bojinov <ignatbojinov@gmail.com>
2023-09-08 20:14:28 +0200

    initalcommit

---
+++ in_class/Lecture_1/Intro/Program.cs
@@ -0,0 +1,69 @@
+﻿using System.ComponentModel.Design;
+using System.Formats.Asn1;
+
+namespace Ignatcho
+{
+
+    class Program
+    {
+        public static void Main()
+        {
+
+            //Data types
+            // Mathematical operation:
+            //*; /; -; +; ++; %; i--; 
+
+            string stringVar = "Hello World!!";
+            int intVar = 100;
+            float floatVar = 10.2f;
+            char charVar = 'A';
+            bool boolVar = true; 
+ 
+            //private variables " _int" 
+
+
+//------------------------------ Ex. 1 ------------------------------
+
+            int a = 5;
+            int b = 10;
+            int c = 20;
+            int sum = a + b + c;
+            Console.WriteLine(sum);
+
+//------------------------------ Ex. 2 ------------------------------
+
+            Console.WriteLine("Enter number: ");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number: ");
+            int number3 =  Convert.ToInt32(Console.ReadLine());
+
+            int moshpit = number1 + number2 + number3;
+            Console.WriteLine($"The sum is: {moshpit}");
+
+//------------------------------ Ex. 3 ------------------------------
+
+            Console.WriteLine("Number 1: ");
+            int num1 = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Number 2: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Number 3: ");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            float avgNum = (num1 + num2 + num3) / 3;
+            Console.WriteLine($"The Average Sum Is {avgNum}");
+
+//------------------------------ Ex. 4 ------------------------------
+
+            Console.WriteLine("Enter radius: ");
+            int radius = Convert.ToInt32(Console.ReadLine());
+
+            double area = Math.PI + radius*radius;
+
+            Console.WriteLine($"The are is: {area}");
+
+
+        }
+    }
+ }
---
+++ in_class/Lecture_2/Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿/* Random dice = new Random(); // Creates a new instance of the Random class to create a new object called dice. 
+int roll = dice.Next(1, 7);
+Console.WriteLine("It's: " + roll); 
+
+
+//-----Overloading methods using the example from above-----
+
+
+Random ddice = new Random();
+int roll1 = ddice.Next();
+int roll2 = ddice.Next(101);
+int roll3 = ddice.Next(50, 101);
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}"); 
+
+
+//----- CODING EXERCISE - Implement a method of the Math class that returns the larger of two numbers -----
+
+int firstValue = 500;
+int secondValue = 600;
+int largerValue = Math.Max(firstValue, secondValue);
+
+Console.WriteLine(largerValue); */
+
+//----- CODING EXERCISE - Write a program that lets you input your name, two numbers and outputs their sum and difference -----
+
+Console.WriteLine("Hello, what's your name: ");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello, {name}, now give me two numbers:");
+int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int sum = num1 + num2;
+int difference = num1 - num2;
+int multi = num1 * num2;
+float sub = num1/num2;
+
+Console.WriteLine($"The sum is: {sum}, and the difference is {difference}");
+Console.WriteLine($"The multiplication is: {multi}, and the subtraction is {sub}");
+
