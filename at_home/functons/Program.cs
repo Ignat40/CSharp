1f4733d380d1f1ce8aeedb3d866fc89bf07a093e
Ignat Bojinov <ignatbojinov@gmail.com>
2023-10-14 19:58:23 +0200

    commit

--- at_home/functons/Program.cs
+++ at_home/functons/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Data;
+using System.Diagnostics.Metrics;
+using System.Net.Http.Headers;
+using System.Security.Cryptography;
 
 namespace Functions
 {
@@ -7,7 +11,99 @@ namespace Functions
     {
         public static void Main()
         {
-            Console.WriteLine("maruli");
+           //PrintNumber();
+
+            //Ex. 2
+            /*
+            Console.WriteLine("Enter a sentance: ");
+            string? input = Console.ReadLine();
+            int count = WordCounter(input);
+            Console.WriteLine(count);
+            
+
+            //Ex. 3
+            Console.WriteLine("Enter value for number 1:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter value for number 2:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter operation:");
+            string operatioin = Console.ReadLine();
+            double result = Calculator(num1, num2, operatioin);
+            Console.WriteLine($"\n Result: {num1} {operatioin} {num2} = {result}");
+
+
+            //Ex. 4
+            Console.WriteLine("Enter string to reverse: ");
+            string? userInput = Console.ReadLine();
+            Console.WriteLine($"Your string is palindrome: {IsPalindrome(userInput)}");
+*/
+
+            //Ex. 5
+            Console.WriteLine("Enter number to check its factorial: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"The factorial of {n} is {Factorial(n)}");
+
+
         }
+        public static void PrintNumber() //FUnctoin that creates a random number
+        {
+    //--------------------- Ex.1 ---------------------\\
+
+            Random number = new();
+            int num = number.Next(1, 11);
+            Console.WriteLine($"Your rndm number is: {num}");
+        }
+        public static int WordCounter(string? input)
+        {
+
+            int wordCounter = input.Split(' ').Length;
+            return wordCounter;
+        }
+
+        public static double Calculator(double num1, double num2, string sign)
+        {
+    //--------------------- Ex.3 ---------------------\\
+
+            if(sign == "+")
+                return num1 + num2;
+            else if(sign == "-")
+                return num1 - num2;
+            else if(sign == "*")
+                return num1 * num2;
+            else if(sign == "/")
+                return num1 / num2;
+            else
+                Console.WriteLine("Invalid operatioin");
+                return 0; 
+        }
+
+        public static bool IsPalindrome(string? input)
+        {
+            string cleanInput = input.Replace(" ", "").ToLower();
+            char[] reversedString = cleanInput.ToCharArray();
+
+            for(int i = 0; i < cleanInput.Length / 2; i++)
+            {
+                if(cleanInput[i] != cleanInput[cleanInput.Length - i - 1])
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static int Factorial(int number)
+        {
+            int f = 1;
+
+            for (int i = 1; i <= number; i++)
+            {
+                f *= i;
+            }
+
+            return f;
+        }
+
+    
+    
     }
 }
--- in_class/Lecture_9.1/Program.cs
+++ in_class/Lecture_9.1/Program.cs
@@ -1,55 +1,93 @@
 ﻿using System;
-using System.Drawing;
-using SkiaSharp;
+using System.Collections.Generic;
 
-class Program
+namespace GuessTheNumberGame
 {
-    static void Main(string[] args)
+    enum Difficulty
     {
-        SKBitmap bitmap = SKBitmap.Decode("/Users/ignat/Desktop/ex2c.PNG");
-
-        if (bitmap == null)
-        {
-            Console.WriteLine("Failed to load the image.");
-        }
-        else
-        {
-            // Resize the image for better ASCII representation
-            int targetWidth = 80; // Set your desired width
-            int targetHeight = (int)(bitmap.Height * (float)targetWidth / bitmap.Width);
-            SKBitmap resizedBitmap = bitmap.Resize(new SKImageInfo(targetWidth, targetHeight), SKFilterQuality.High);
-
-            // Convert the image to ASCII characters
-            string asciiArt = ConvertToAscii(resizedBitmap);
-
-            // Print the ASCII art to the console
-            Console.WriteLine(asciiArt);
-        }
+        Easy = 10,
+        Medium = 20,
+        Hard = 50
     }
 
-    static string ConvertToAscii(SKBitmap bitmap)
+    struct GameScore
     {
-        char[] asciiChars = { ' ', '.', ':', '-', '=', '+', '*', '#', '%', '8', '@' };
-        string asciiArt = "";
+        public int TotalGuesses;
+        public int CorrectGuesses;
+    }
 
-        for (int y = 0; y < bitmap.Height; y++)
+    class Review5
+    {
+        static void Main()
         {
-            for (int x = 0; x < bitmap.Width; x++)
+            GameScore score = new() { TotalGuesses = 0, CorrectGuesses = 0 };
+
+             
+
+            while (true)
             {
-                // Get the grayscale intensity of the pixel
-                SKColor pixelColor = bitmap.GetPixel(x, y);
-                int intensity = (int)(0.299 * pixelColor.Red + 0.587 * pixelColor.Green + 0.114 * pixelColor.Blue);
+                Console.WriteLine("Select difficulty: Easy, Medium, Hard or Exit to quit.");
+                string? input = Console.ReadLine();
+                if (input?.ToLower() == "exit") break;
 
-                // Map grayscale intensity to ASCII characters
-                int index = intensity * (asciiChars.Length - 1) / 255;
-                char asciiChar = asciiChars[index];
-
-                // Append the ASCII character to the ASCII art
-                asciiArt += asciiChar;
+                if (Enum.TryParse(input, true, out Difficulty difficulty))
+                {
+                    if(Enum.IsDefined(typeof(Difficulty), (int)(difficulty)))
+                    {
+                        StartNewRound(ref score, difficulty);
+                    }
+                }
             }
-            asciiArt += "\n"; // Newline after each row
         }
 
-        return asciiArt;
+        static void StartNewRound(ref GameScore score, Difficulty difficulty)
+        {
+            int targetNumber = GenerateRandomNumber(difficulty);
+            while (true)
+            {
+                Console.WriteLine($"Guess the number between 1 and {(int)difficulty} or Exit to exit.");
+                string? guessInput = Console.ReadLine();
+                if (guessInput?.ToLower() == "exit") Environment.Exit(0);
+                if (int.TryParse(guessInput, out int guess))
+                {
+                    if (EvaluateGuess(targetNumber, guess, ref score))
+                    {
+                        Console.WriteLine("You guessed it!");
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Try again.");
+                }
+            }
+        }
+
+        static int GenerateRandomNumber(Difficulty difficulty)
+        {
+            Random random = new();
+            return random.Next(1, (int)difficulty + 1);
+        }
+
+        static bool EvaluateGuess(int target, int guess, ref GameScore score)
+        {
+            bool isCorrect = target == guess;
+            score.TotalGuesses++;
+
+
+            if (isCorrect)
+            {
+                score.CorrectGuesses++;
+                Console.WriteLine("Correct!");
+            }
+            else
+            {
+                Console.WriteLine(target > guess ? "Too low!" : "Too high!");
+            }
+
+            Console.WriteLine($"Your score: {score.CorrectGuesses} / {score.TotalGuesses}");
+
+            return isCorrect;
+        }
     }
 }
