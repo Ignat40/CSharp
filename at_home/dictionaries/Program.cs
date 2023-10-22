e18ba5cb0b0d4d3a9dcff64497632d8b45b8b017
Ignat Bojinov <ignatbojinov@gmail.com>
2023-10-22 13:27:51 +0200

    dictionaries

---
+++ at_home/dictionaries/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Dictionaries
+{
+
+    //key - stores a value 
+    //Auto - car
+    class Program
+    {
+        public static void Main()
+        {
+            Dictionary<int, string> myDict = new Dictionary<int, string>()
+            {
+                {1, "Mustard"},
+                {2, "Mayo"},
+                {3, "Ketchup"},
+            };
+            
+        }
+    }
+}
