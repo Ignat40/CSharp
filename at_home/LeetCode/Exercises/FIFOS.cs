ccb86f50647af068f131e2ec612e6ab7fb571874
Ignat Bojinov <ignatbojinov@gmail.com>
2024-02-01 13:13:32 +0100

    study KMP to solve (unsolved)

---
+++ at_home/LeetCode/Exercises/FIFOS.cs
@@ -0,0 +1,20 @@
+namespace LeetCode
+{
+    public class FindOccurence
+    {
+        // public static bool StrStr(string haystack, string needle)
+        // {
+        //     bool isFound = false;
+
+        //     for (int i = 0; i < haystack.Length; i++)
+        //     {
+        //         for (int j = 0; j < needle.Length; j++)
+        //         {
+        //             if (haystack[i] == needle[j])
+        //                 isFound = true;
+        //         }
+
+        //     }
+        // }
+    }
+}
--- at_home/LeetCode/Exercises/Program.cs
+++ at_home/LeetCode/Exercises/Program.cs
@@ -4,9 +4,7 @@
     {
         public static void Main()
         {
-            int[] arrayNum = {3, 2, 2, 3};
-            int val = 3;
-            Console.WriteLine(RemoveElement.RemEl(arrayNum, val));
+            //Console.WriteLine(FindOccurence.StrStr("sadbutsad", "sad"));
         }
     }
 }
