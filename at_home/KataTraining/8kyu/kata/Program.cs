b8d8f6bc77d8398ac2037fa1dd2d03bdbb6b0d84
Ignat Bojinov <ignatbojinov@gmail.com>
2024-02-17 19:33:52 +0100

    kata

--- at_home/KataTraining/8kyu/kata/Program.cs
+++ at_home/KataTraining/8kyu/kata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Net.Http.Headers;
 using System.Reflection.Metadata;
 
 namespace Kata8
@@ -8,12 +9,7 @@ namespace Kata8
     {
         public static void Main()
         {
-            int[] test1 = {0};
-            int[] test2 = {0, 1, 4};
-            int[] test3 = {0, -1, -5};
-            Console.WriteLine(OddOrEven(test1));
-            Console.WriteLine(OddOrEven(test2));
-            Console.WriteLine(OddOrEven(test3));
+            Digitize(123455);
         }
 
         public static string SentanceSmash(string[] words)
@@ -57,39 +53,39 @@ namespace Kata8
 
         public static int Min(int[] list)
         {
-        //------------------------- Ex. 4 -------------------------\\
+            //------------------------- Ex. 4 -------------------------\\
 
             int min = list[0];
             for (int i = 0; i < list.Length; i++)
             {
-                if(min > list[i])
+                if (min > list[i])
                 {
                     min = list[i];
-                }                
+                }
             }
             return min;
         }
 
         public static int Max(int[] list)
         {
-        //------------------------- Ex. 5 -------------------------\\
+            //------------------------- Ex. 5 -------------------------\\
 
             int max = list[0];
             for (int i = 0; i < list.Length; i++)
             {
-                if(max < list[i])
+                if (max < list[i])
                 {
                     max = list[i];
-                }                
+                }
             }
             return max;
         }
 
         public static string AreYouPlayingBanjo(string name)
         {
-        //------------------------- Ex. 6 -------------------------\\
+            //------------------------- Ex. 6 -------------------------\\
 
-            if(name[0] == 'R' || name[0] == 'r')
+            if (name[0] == 'R' || name[0] == 'r')
                 return name + "plays banjo";
             else
                 return name + "does not play banjo";
@@ -97,11 +93,11 @@ namespace Kata8
 
         public static double SumArray(double[] arr)
         {
-        //------------------------- Ex. 7 -------------------------\\
+            //------------------------- Ex. 7 -------------------------\\
 
             double sum = 0;
 
-            if(arr.Length <= 0)
+            if (arr.Length <= 0)
             {
                 return 0;
             }
@@ -119,18 +115,18 @@ namespace Kata8
 
         public static string DoubleChar(string s)
         {
-        //------------------------- Ex. 8 -------------------------\\
+            //------------------------- Ex. 8 -------------------------\\
             string doubleString = string.Empty;
 
             for (int i = 0; i < s.Length; i++)
             {
-                doubleString += s[i]+s[i].ToString();
+                doubleString += s[i] + s[i].ToString();
             }
             return doubleString;
         }
         public static string dnaToRna(string dna)
         {
-        //------------------------- Ex. 8 -------------------------\\
+            //------------------------- Ex. 8 -------------------------\\
             // string newString = dna.Replace('T', 'U');
             // return newString;
 
@@ -138,7 +134,7 @@ namespace Kata8
 
             for (int i = 0; i < charArray.Length; i++)
             {
-                if(charArray[i] == 'T')
+                if (charArray[i] == 'T')
                 {
                     charArray[i] = 'U';
                 }
@@ -146,15 +142,15 @@ namespace Kata8
 
             string rna = new string(charArray);
             return rna;
-        
+
         }
 
         public static int[] Maps(int[] x)
         {
-        //------------------------- Ex. 9 -------------------------\\
+            //------------------------- Ex. 9 -------------------------\\
             int[] xDoubled = new int[x.Length];
 
-            for(int i = 0; i < x.Length; i++)
+            for (int i = 0; i < x.Length; i++)
             {
                 xDoubled[i] = x[i] * 2;
             }
@@ -165,43 +161,63 @@ namespace Kata8
 
         public static int Getsum(int x, int b)
         {
-        //------------------------- Ex. 10 -------------------------\\
+            //------------------------- Ex. 10 -------------------------\\
             bool isEqual = false;
             int sum = 0;
 
-            if(x < b)
+            if (x < b)
             {
                 for (int i = x; i <= b; i++)
                 {
                     sum += i;
                 }
             }
-            else if(x > b)
+            else if (x > b)
             {
                 for (int i = b; i <= x; i++)
                 {
                     sum += i;
-                }  
+                }
             }
             else
             {
-                sum = x; 
-            } 
+                sum = x;
+            }
 
             return sum;
-            
+
         }
 
         public static string OddOrEven(int[] arr)
-        {   
+        {
+            //------------------------- Ex. 10 -------------------------\\
+
             int sum = 0;
-            for(int i = 0; i < arr.Length; i++)
+            for (int i = 0; i < arr.Length; i++)
             {
                 sum += arr[i];
             }
 
             var test = sum % 2 == 0 ? "even" : "odd";
             return test;
+        }
+
+        public static long[] Digitize(long n)
+        {
+            //------------------------- Ex. 10 -------------------------\\
+
+            int counter = 0;
+            string intputToString = n.ToString();
+            long[] reverseDigits = new long[intputToString.Length];
+
+
+            for (int i = intputToString.Length - 1; i >= 0; --i)
+            {
+                reverseDigits[counter] = long.Parse(intputToString[i].ToString());
+                counter++;
+            }
+
+            return reverseDigits;
         }
     }
 }
