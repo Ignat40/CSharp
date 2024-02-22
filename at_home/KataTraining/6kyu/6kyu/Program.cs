63f51138d9b2507114029f8498863a0e495c4bf4
Ignat Bojinov <ignatbojinov@gmail.com>
2024-02-22 18:54:59 +0100

    Make a Window Problem

---
+++ at_home/KataTraining/6kyu/6kyu/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Kata
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine(MakeAWindow(6));
+            // ---------
+            // |...|...|
+            // |...|...|
+            // |...|...|
+            // |---+---|
+            // |...|...|
+            // |...|...|
+            // |...|...|
+            // ---------
+
+        }
+
+        public static string MakeAWindow(int num)
+        {
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", num * 2 + 3)));
+
+            for (int i = 0; i < 3; i++)
+            {
+                while (i < num)
+                {
+                    Console.Write("|" + string.Concat(Enumerable.Repeat(".", num)));
+                    Console.WriteLine("|" + string.Concat(Enumerable.Repeat(".", num)) + "|");
+                    i++;
+                }
+
+            }
+
+            Console.WriteLine("|" + string.Concat(Enumerable.Repeat("-", num)) + "+" + string.Concat(Enumerable.Repeat("-", num)) + "|");
+
+            for (int i = 0; i < 3; i++)
+            {
+                while (i < num)
+                {
+                    Console.Write("|" + string.Concat(Enumerable.Repeat(".", num)));
+                    Console.WriteLine("|" + string.Concat(Enumerable.Repeat(".", num)) + "|");
+                    i++;
+                }
+
+            }
+            Console.WriteLine(string.Concat(Enumerable.Repeat("-", num * 2 + 3)));
+
+            return string.Empty;
+        }
+    }
+}
